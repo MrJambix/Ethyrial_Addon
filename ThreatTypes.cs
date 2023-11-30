3feac4905379c302b7951ea7c524631fb56fb996
MrJambix <131601090+MrJambix@users.noreply.github.com>
2023-11-30 23:40:49 +0100

    Create ThreatTypes.cs

---
+++ ThreatTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RPGLibrary
+{
+	// Token: 0x020000ED RID: 237
+	public enum ThreatTypes
+	{
+		// Token: 0x0400051B RID: 1307
+		Offensive,
+		// Token: 0x0400051C RID: 1308
+		Defensive,
+		// Token: 0x0400051D RID: 1309
+		Support
+	}
+}
